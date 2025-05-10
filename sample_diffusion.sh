24b4ba13b2a6d5af8712d317d99279a9173f6031
JiashiYin <jyin15@illinois.edu>
2025-05-09 23:14:18 -0500

    Uploaded a wrong bash script

--- sample_diffusion.sh
+++ sample_diffusion.sh
@@ -5,7 +5,7 @@ MODEL_FLAGS="--image_size 256 --num_channels 128 --class_cond False --num_res_bl
 DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False"
 
 # Run the sampling script
-python /scratch/bcsl/jyin15/DiffBlend/scripts/inpainting_sample_uses_interface.py \
+python /scratch/bcsl/jyin15/DiffBlend/scripts/sample_new.py \
   --data_dir /scratch/bcsl/jyin15/DiffBlend/toy_data \
   --log_dir /scratch/bcsl/jyin15/DiffBlend/sampling_results_playground \
   --model_path /scratch/bcsl/jyin15/DiffBlend/ckpt/emasavedmodel_0.9999_482000.pt \
